e04b403720af703912455f7bf04b115099c2bc7a
Stezkoy <stezko@gmail.com>
2026-05-14 13:32:22 +0500

    Start

---
+++ README.md
@@ -0,0 +1,6 @@
+# GreenUi Theme for Flarum v2
+
+## Install  
+```
+composer require stezkoy/flarum-green2ui
+```
---
+++ extend.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace stezkoy\flarumgreen2ui;
+
+use Flarum\Extend;
+use Flarum\Frontend\Document;
+
+return [
+    (new Extend\Frontend('forum'))
+        ->css(__DIR__.'/less/forum.less'),  
+    (new Extend\Frontend('admin'))
+        ->css(__DIR__.'/less/admin.less'),  
+];
