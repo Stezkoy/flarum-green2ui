7c0c2fc0c23ead812963fc814da835cbe8cd130f
Stezkoy <stezko@gmail.com>
2026-05-14 14:08:59 +0500

    0.2

--- extend.php
+++ extend.php
@@ -1,13 +1,11 @@
 <?php
 
-namespace stezkoy\flarumgreen2ui;
-
 use Flarum\Extend;
 use Flarum\Frontend\Document;
 
 return [
     (new Extend\Frontend('forum'))
-        ->css(__DIR__.'/less/forum.less'),  
+        ->css(__DIR__.'/less/forum.less'),
     (new Extend\Frontend('admin'))
-        ->css(__DIR__.'/less/admin.less'),  
+        ->css(__DIR__.'/less/admin.less'),
 ];
